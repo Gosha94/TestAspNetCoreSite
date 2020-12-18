6e9b2fb5551baa9642b62425220007d1378d9ff5
Gosha94 <gerizch@rambler.ru>
2020-12-18 21:49:06 +0300

    Добавьте файлы проекта.

---
+++ TestAspNetCoreSite/Startup.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TestAspNetCoreSite
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Подключаем MVC к проекту
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Подключаем вывод исключений в режиме разработки
+            app.UseDeveloperExceptionPage();
+            // Подключаем вывод кодов ошибок на страницах
+            app.UseStatusCodePages();
+            // Включаем поддержку стат.файлов в проекте (картинки и т.д.)
+            app.UseStaticFiles();
+            // Включаем отслеживание URL адреса, по которому переходят
+            // Если не указывается конкретный адрес ресурса, будет передаваться осн контролер по умолчанию index.html
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
