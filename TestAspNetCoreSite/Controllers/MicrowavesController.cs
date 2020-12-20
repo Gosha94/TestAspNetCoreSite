0de6aa6ad5c3cfcf5517af47c4794b4311dd40bf
Gosha94 <gerizch@rambler.ru>
2020-12-20 20:00:02 +0300

    Разбиение Razor шаблонов на более мелкие  только с нужными  данными

--- TestAspNetCoreSite/Controllers/MicrowavesController.cs
+++ TestAspNetCoreSite/Controllers/MicrowavesController.cs
@@ -7,7 +7,6 @@ namespace TestAspNetCoreSite.Controllers
     public class MicrowavesController : Controller
     {
         // Функции контроллера возвращают результирующую html страницу в виде ViewResult
-
         private readonly IAllMicrowaves _allMicrowaves;
         private readonly IMicrowavesCategory _allMicrowavesCategory;
 
@@ -21,6 +20,7 @@ namespace TestAspNetCoreSite.Controllers
             // Для передачи данных внутрь Html, не рекомендуется использовать, исп модели данных
             //ViewBag.Category = "Любой текст";
             //var microwaves = this._allMicrowaves.Microwaves;
+            ViewBag.Title = "Лучшие микроволновки 2021";
             MicrowavesListViewModel micrVm = new MicrowavesListViewModel();
             micrVm.AllMicrowaves = _allMicrowaves.Microwaves;
             micrVm.CurrentCategory = "Микроволновки";
